0d2d6e0db8a901abab45da1c306a34ab2f0d91ad
devlalo <lalo1isc@gmail.com>
2025-05-09 12:03:07 -0600

    creación de controlador REST en Spring Boot

--- src/main/java/com/platzi/market/domain/service/ProductService.java
+++ src/main/java/com/platzi/market/domain/service/ProductService.java
@@ -4,6 +4,8 @@ import com.platzi.market.domain.Product;
 import com.platzi.market.domain.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductService {
--- src/main/java/com/platzi/market/persistence/entity/Compra.java
+++ src/main/java/com/platzi/market/persistence/entity/Compra.java
@@ -29,8 +29,8 @@ public class Compra {
     @JoinColumn(name = "id_cliente", insertable = false, updatable = false)
     private Cliente cliente;
 
-    @OneToMany(mappedBy = "producto")
-    private List<ComprasProducto> producto;
+    @OneToMany(mappedBy = "compra")
+    private List<ComprasProducto> productos;
     
     public Integer getIdCompra() {
         return idCompra;
